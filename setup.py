798ac257edc5b792860570eacd6d81d801543849
Carl0000oak <149906774+Carl0000oak@users.noreply.github.com>
2026-06-22 15:17:12 -0300

    🔧 Criando __init__.py e setup.py corretos

---
+++ setup.py
@@ -0,0 +1,13 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name="apk_quim_plant",
+    version="1.0.0",
+    author="Carl",
+    description="Analise Fitoquimica de Plantas",
+    packages=find_packages(include=['complant', 'complant.*']),
+    install_requires=[
+        "requests>=2.31.0",
+    ],
+    python_requires=">=3.8",
+)
